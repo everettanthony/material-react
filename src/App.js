180ec308a4dbbea3707fbbb4ea673ba095ead77e
Bradshaw <Tony.Bradshaw@cmc.com>
2019-05-07 10:53:48 -0500

    First push up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Button from "@material-ui/core/Button";
+import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { withStyles } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
 
-function App() {
+const styles = theme => ({
+  root: {
+    color: theme.palette.text.primary,
+  },
+  icon: {
+    color: indigo[500],
+    margin: theme.spacing.unit, 
+    fontSize: 32,
+  },
+  link: {
+    margin: theme.spacing.unit,
+  },
+});
+
+const dudUrl = 'javascript:;';
+
+function App(props) {
+  const { classes } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      {
+        <div className="App">
+          <Typography variant="h2" gutterBottom>h2. Heading</Typography>
+          <Grid container className={classes.root}>
+            <Grid item xs={4}>
+              <Typography>Filled</Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <DeleteIcon className={classes.icon} />
+              <DeleteForeverIcon className={classes.icon} />
+            </Grid>
+          </Grid>
+          <Typography>
+            <Link href="http://google.com" className={classes.link}>Link</Link>
+            <Link href={dudUrl} color="inherit" className={classes.link}>{'color="inherit"'}</Link>
+            <Link href={dudUrl} variant="body1" className={classes.link}>{'variant="body1"'}</Link>
+            <Link component="button" variant="body2" onClick={() => { alert("I'm a button."); }}>Button Link</Link>
+          </Typography>
+        </div>
+      }
+    </React.Fragment>
   );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
